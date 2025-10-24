09c026aa800e3cd9fa5b593b08486ba3a996bb4d
mathis marschutz <mathis.marschutz@epfedu.fr>
2025-10-24 11:41:14 +0200

    fin hello word

---
+++ TP0_HelloWord/src/tp0_helloword/TP0_HelloWord.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloword;
+
+import java.util.Scanner;
+
+/**Marschutz
+ *tp0 pour se familiariser
+ * 20/10/2025
+ * @author mmars
+ */
+public class TP0_HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ;
+         System.out.println( " C'est ciao " ) ;
+         
+         
+Scanner sc ;
+sc = new Scanner (System.in) ;
+System.out.println("Quel est votre prénom ?");
+sc.nextLine(); 
+    }
+    
+}
